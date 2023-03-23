54812c46c405056e927d0300608877600cc2f9e1
furkantunc3r <furkan_tuncer@yahoo.com>
2023-03-23 08:27:30 -0400

    goldeN

---
+++ Makefile
@@ -0,0 +1,15 @@
+all:
+	@mkdir -p /${HOME}/data/db 
+	@mkdir -p /${HOME}/data/wordpress
+	@docker-compose -f ./srcs/docker-compose.yaml up -d --build
+
+volume_rm:
+	docker volume rm srcs_DB srcs_WordPress
+
+clean:
+	@docker stop mariadb nginx wordpress
+	@docker rm mariadb nginx wordpress
+	@docker image rm srcs_nginx srcs_mariadb srcs_wordpress
+	@sudo rm -rf /${HOME}/data/db /${HOME}/data/wordpress
+	docker system prune -af
+	docker volume rm srcs_DB srcs_WordPress
--- srcs/requirements/wordpress/tools/wpinstall.sh
+++ srcs/requirements/wordpress/tools/wpinstall.sh
@@ -6,7 +6,12 @@ wp core download --allow-root
 
 wp config create --allow-root --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${USER} --dbpass=${DB_PASS} --dbcharset=utf8
 
-wp core install --allow-root --url=https://localhost --title="Furkan TUNCER" --admin_name=${USER} --admin_password=${ADMIN_PASS} --admin_email=${ADMIN_EMAIL}
+wp core install --allow-root --url=https://ftuncer.42.fr --title="Furkan TUNCER" --admin_name=${USER} --admin_password=${ADMIN_PASS} --admin_email=${ADMIN_EMAIL}
+
+wp user create apollo11 apollo11@gmail.com --role=subscriber --user_pass=gizli --description="Space-ship and human" --display_name=APOLLO11 --allow-root 
+
+wp db optimize --allow-root
+
 wp theme install microtype --activate --allow-root
 
 service php7.3-fpm start --allow-root
